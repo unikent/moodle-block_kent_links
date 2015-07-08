1189b7e5f110aa34dc46f2eda52565682be9884a
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-07-08 08:07:52 +0100

    Fix #5

--- classes/observers.php
+++ classes/observers.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace block_kent_course_overview;
+namespace block_kent_links;
 
 defined('MOODLE_INTERNAL') || die();
 
